10b79aff9ea4cbc5d3e31b3917573b11ab1cc611
sne <igordnepr27@gmail.com>
2025-08-12 01:48:10 +0200

    тепер повертає тільки error

--- signer.go
+++ signer.go
@@ -1,6 +1,9 @@
 package crypto
 
-import "crypto/ed25519"
+import (
+	"crypto/ed25519"
+	"fmt"
+)
 
 // TODO: зробити перевірку ключа, і повертати помилку, в разі проблем
 
@@ -8,6 +11,10 @@ func Sign(priv []byte, message []byte) ([]byte, error) {
 	return ed25519.Sign(priv, message), nil
 }
 
-func Verify(pub []byte, message []byte, sig []byte) (bool, error) {
-	return ed25519.Verify(pub, message, sig), nil
+func Verify(pub []byte, message []byte, sig []byte) error {
+	res := ed25519.Verify(pub, message, sig)
+	if !res {
+		return fmt.Errorf("not valid signature")
+	}
+	return nil
 }
