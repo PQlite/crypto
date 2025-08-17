f46da14ce8ac2da4e374c028f5ba1ccd20d5b893
sne <igordnepr27@gmail.com>
2025-08-17 20:45:14 +0200

    додав перевірку на розмір ключів

--- signer.go
+++ signer.go
@@ -8,10 +8,16 @@ import (
 // TODO: зробити перевірку ключа, і повертати помилку, в разі проблем
 
 func Sign(priv []byte, message []byte) ([]byte, error) {
+	if len(priv) != ed25519.PrivateKeySize {
+		return nil, fmt.Errorf("помилка розміру приватного ключа, треба: %d, отримано: %d", ed25519.PrivateKeySize, len(priv))
+	}
 	return ed25519.Sign(priv, message), nil
 }
 
 func Verify(pub []byte, message []byte, sig []byte) error {
+	if len(pub) != ed25519.PublicKeySize {
+		return fmt.Errorf("помилка розміру публічного ключа, треба: %d, отримано: %d", ed25519.PublicKeySize, len(pub))
+	}
 	res := ed25519.Verify(pub, message, sig)
 	if !res {
 		return fmt.Errorf("not valid signature")
